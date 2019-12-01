e36874a69f57221fac5e28ff547623aacf3aed07
jeremy9682 <jeremy9682@gmail.com>
2019-11-30 19:35:04 -0500

    test github 1st

---
+++ app/src/main/java/com/example/rankmystore/Review.kt
@@ -0,0 +1,22 @@
+package com.example.rankmystore
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast
+import android.view.View.OnClickListener
+
+class Review  : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review)
+
+
+
+
+    }
+}
