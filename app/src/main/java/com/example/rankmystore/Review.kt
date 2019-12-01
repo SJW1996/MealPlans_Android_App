cc818ab46204bf9d4ba63ca17acf3f724c2029fa
jeremy9682 <jeremy9682@gmail.com>
2019-12-01 14:47:27 -0500

    review update 12.1

--- app/src/main/java/com/example/rankmystore/Review.kt
+++ app/src/main/java/com/example/rankmystore/Review.kt
@@ -15,8 +15,21 @@ class Review  : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
 
+        var add_Photo_Button = findViewById<View>(R.id.addStorePhotosButton)
+        var done_Button = findViewById<View>(R.id.submitButton)
+        var storeName = findViewById<View>(R.id.storeName)
+        var address = findViewById<View>(R.id.Address)
+        var comments = findViewById<View>(R.id.Comments)
+
+        done_Button.setOnClickListener(){finishAddPhotos()}
 
 
 
+
+    }
+
+    fun finishAddPhotos(){
+        var intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
