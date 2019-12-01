0c36c25049fd2c707d309cd0f56398bde3cdac9d
CS436finalProject <1652765684@qq.com>
2019-12-01 17:06:45 -0500

    finish login and register

--- app/src/main/java/com/example/rankmystore/login.kt
+++ app/src/main/java/com/example/rankmystore/login.kt
@@ -14,38 +14,30 @@ import android.view.View.OnClickListener
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 
 class login : AppCompatActivity(){
-    private lateinit var mAuth: FirebaseAuth
+    //private lateinit var mAuth: FirebaseAuth
+    lateinit var mDatabase: FirebaseDatabase
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance()
+
         var input_email = findViewById<View>(R.id.input_email) as EditText
         var input_password = findViewById<View>(R.id.input_password) as EditText
-        val email = input_email.text.toString()
-        var password = input_password.text.toString()
+       // val email = input_email.text.toString()
+       // var password = input_password.text.toString()
+
 
         var btn_reset = findViewById<View>(R.id.btn_reset) as Button
         var btn_submit = findViewById<View>(R.id.btn_submit) as Button
 
         var sign_up_button = findViewById<View>(R.id.btn_sign_up) as Button
 
-        if (!email.isEmpty() && !password.isEmpty()){
-            this.mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, OnCompleteListener<AuthResult>{
-                task ->
-                    if (task.isSuccessful){
-                        //go to which activity
-                        startActivity(Intent(this, Profile::class.java))
-                        Toast.makeText(this, "Successfully Login in to account", Toast.LENGTH_LONG).show()
-                    }else{
-                        Toast.makeText(this, "Error Login in", Toast.LENGTH_SHORT).show()
-                    }
-            })
-        }else{
-            Toast.makeText(this, "Please fill up the Login Information", Toast.LENGTH_SHORT).show()
-        }
 
         btn_reset.setOnClickListener{
             input_email.setText("")
@@ -53,12 +45,33 @@ class login : AppCompatActivity(){
         }
 
         btn_submit.setOnClickListener{
-            val user_name = input_email.text
-            val password = input_password.text
-            Toast.makeText(this@login,  user_name,Toast.LENGTH_LONG).show()
+            login_user()
+            //Toast.makeText(this@login,  input_email,Toast.LENGTH_LONG).show()
         }
 
         sign_up_button.setOnClickListener({gotoregisterActivity()})
+    }
+
+    private fun login_user(){
+        var input_email = findViewById<View>(R.id.input_email) as EditText
+        var input_password = findViewById<View>(R.id.input_password) as EditText
+        val email = input_email.text.toString()
+        var password = input_password.text.toString()
+
+        if (!email.isEmpty() && !password.isEmpty()){
+            this.mAuth!!.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, OnCompleteListener<AuthResult>{
+                    task ->
+                if (task.isSuccessful){
+                    //go to which activity
+                    startActivity(Intent(this, Profile::class.java))
+                    Toast.makeText(this, "Successfully Login in to account", Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(this, "Error Login in", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }else{
+            Toast.makeText(this, "Please fill up the Login Information", Toast.LENGTH_SHORT).show()
+        }
     }
     fun gotoregisterActivity(){
         val register_intent = Intent(this, register::class.java)
--- app/src/main/java/com/example/rankmystore/register.kt
+++ app/src/main/java/com/example/rankmystore/register.kt
@@ -14,45 +14,61 @@ import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_profile.*
+//import jdk.nashorn.internal.runtime.ECMAException.getException
+import com.google.firebase.auth.FirebaseAuthException
+import android.util.Log
+import com.google.firebase.FirebaseNetworkException
 
 class register : AppCompatActivity(){
-    private lateinit var mAuth: FirebaseAuth
+    //private lateinit var mAuth: FirebaseAuth
+   // val mAuth = FirebaseAuth.getInstance()
+    lateinit var mDatabase: FirebaseDatabase
+    private var mAuth: FirebaseAuth? = null
 
-    lateinit var mDatabase : DatabaseReference
+    // lateinit var mDatabase : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
 
+        mAuth = FirebaseAuth.getInstance()
+
+        mDatabase = FirebaseDatabase.getInstance()
+        var register_button = findViewById<View>(R.id.register_btn) as Button
+        register_button.setOnClickListener({register_new_user()})
+        //Go back to the login page
+        var login_button = findViewById<View>(R.id.tvlogin) as Button
+        login_button.setOnClickListener({goto_Loginactivity()})
+
+
+
+    }
+
+    private fun register_new_user(){
 
         val emailText = findViewById<View>(R.id.etusername) as EditText
         val passWordText = findViewById<View>(R.id.etpassword) as EditText
 
         var email = emailText.text.toString()
         var password = passWordText.text.toString()
-        //Go back to the login page
-        var login_button = findViewById<View>(R.id.tvlogin) as Button
-        login_button.setOnClickListener({goto_Loginactivity()})
-
         if (!email.isEmpty() && !password.isEmpty()){
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, OnCompleteListener { task ->
+            mAuth!!.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, OnCompleteListener { task ->
                 if (task.isSuccessful){
-                    val user = mAuth.currentUser
+                    val user = mAuth!!.currentUser
                     val uid = user!!.uid
                     //adding review
                     //mDatabase.child(uid).child("Name").setValue(name)
-                    startActivity(Intent(this,Profile::class.java))
+                    startActivity(Intent(this,login::class.java))
                     Toast.makeText(this, "Successfully registered", Toast.LENGTH_LONG).show()
 
                 }else{
+                    val e = task.exception as FirebaseNetworkException
+                    Log.e("Register activity", "Failed Registration", e)
                     Toast.makeText(this, "Error registering, please try again", Toast.LENGTH_LONG).show()
                 }
             })
         }else{
             Toast.makeText(this, "Please fill up correct information", Toast.LENGTH_LONG).show()
         }
-
-
-
     }
     fun goto_Loginactivity(){
         val intent = Intent(this, login::class.java)
